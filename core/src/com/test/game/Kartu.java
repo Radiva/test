3827a6c0c4914badf24a067c375a0187e6582f5f
radiva <28networking@gmail.com>
2016-10-20 07:49:25 +0700

    Versi 0.1.5

---
+++ core/src/com/test/game/Kartu.java
@@ -0,0 +1,22 @@
+package com.test.game;
+
+/**
+ * Created by radiva on 20/10/16.
+ */
+public class Kartu {
+    public void deck() {
+
+    }
+
+    public void shuffle() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void sisa() {
+        
+    }
+}
